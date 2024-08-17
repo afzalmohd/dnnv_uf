5f3392b81ab98d5fbf0302778e375ad9a1678c46
Mohammad Afzal <afzal7242mohd@gmail.com>
2024-08-17 22:33:01 +0530

    added

--- generate_instance_file.py
+++ generate_instance_file.py
@@ -2,7 +2,7 @@ import csv
 import os
 
 
-file_path = '/home/afzal/tools/networks/conf_final/simple_conf/instances.csv'
+file_path = '/home/afzal/tools/networks/conf_final/simple_conf1/instances.csv'
 timeout = 2000
 def write_to_file(net_path, prop_path):
     with open(file_path, 'a') as f:
@@ -11,8 +11,8 @@ def write_to_file(net_path, prop_path):
 
 
 
-net_dir = '/home/afzal/tools/networks/conf_final/eran_mod_simple_conf'
-prop_dir = '/home/afzal/tools/networks/conf_final/simple_conf/props'
+net_dir = '/home/afzal/tools/networks/conf_final/eran_mod_simple_conf1'
+prop_dir = '/home/afzal/tools/networks/conf_final/props'
 
 nets = ['mnist_relu_3_50.onnx', 'mnist_relu_3_100.onnx', 'mnist_relu_5_100.onnx', 'mnist_relu_6_100.onnx']
 nets += ['mnist_relu_6_200.onnx', 'mnist_relu_9_100.onnx', 'mnist_relu_9_200.onnx']
