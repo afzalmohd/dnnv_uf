e261b85e853b06d857c60fe4fffaa2c0fd41151a
Mohammad Afzal <afzal7242mohd@gmail.com>
2024-10-17 00:17:29 +0530

    m1

--- abcrown_tool_run_scripts/script.py
+++ abcrown_tool_run_scripts/script.py
@@ -183,6 +183,7 @@ def print_cmnds_abcrowns(num_cpu, log_dir, dataset='MNIST'):
 
 if __name__ == '__main__':
     dataset = 'MNIST' # 'MNIST'/'CIFAR10'
+    dataset = 'CIFAR10'
     if len(sys.argv) == 3:
         num_cpu = int(sys.argv[1])
         log_dir = sys.argv[2]
