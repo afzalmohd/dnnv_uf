b0e439b0909d6d9f44460faca8e96ae86576c3c5
afzal <afzal7242mohd@gmail.com>
2025-04-29 19:45:08 +0530

    m

--- generate_benchmarks/fp.py
+++ generate_benchmarks/fp.py
@@ -4,6 +4,7 @@ import yaml
 import numpy as np
 import shutil
 import onnxruntime as ort
+import torch
 from torchvision import transforms
 from torch.utils.data import DataLoader
 from torchvision.datasets import MNIST
@@ -11,6 +12,7 @@ from generate_benchmarks.setup import clean_directory, get_output_dims, set_imag
 from generate_benchmarks.generate_properties import create_input_bounds_tf, save_vnnlib_tf_standard, save_vnnlib_oracle_guided
 from generate_benchmarks.modify_onnx_fp import update_fc_relu_oracle
 
+softmax = torch.nn.Softmax(dim=1)
 # transform = transforms.Compose([
 #     transforms.ToTensor(), 
 #     # transforms.Normalize((0.1307,), (0.3081,)),
@@ -22,21 +24,50 @@ from generate_benchmarks.modify_onnx_fp import update_fc_relu_oracle
 # dataset = MNIST(root='../data', train=is_training_dataset, download=True, transform=transform)
 # dataset_loader = DataLoader(dataset, batch_size=batch_size, shuffle=False)
 
-def gen_props_standard(spec_dir, selected_images, selected_labels, selected_idxs, eps):
+def gen_props_standard(spec_dir, selected_images, selected_labels, selected_idxs, eps, dataset='MNIST'):
+    if dataset == 'CIFAR10':
+        gen_props_standard_cifar10(spec_dir, selected_images, selected_labels, selected_idxs, eps)
+    else:
+        counter = 0
+        for i in range(len(selected_images)):
+            image = selected_images[i]
+            image = image.reshape(784)
+            label = selected_labels[i]
+            idx = selected_idxs[i]
+            for ep in eps:
+                lb,ub = create_input_bounds_tf(image, ep)
+                spec_path = f"prop_{idx}_{ep}.vnnlib"
+                spec_path = os.path.join(spec_dir, spec_path)
+                save_vnnlib_tf_standard(lb, ub, label, spec_path, dataset)
+                counter += 1
+
+        print(f"Total number of props: {counter}")
+
+def gen_props_standard_cifar10(spec_dir, selected_images, selected_labels, selected_idxs, eps):
+    mean = np.array([0.4914,0.4822,0.4465])
+    std = np.array([0.2470,0.2435,0.2616])
+    dataset = 'CIFAR10'
     counter = 0
     for i in range(len(selected_images)):
         image = selected_images[i]
-        image = image.reshape(784)
         label = selected_labels[i]
         idx = selected_idxs[i]
         for ep in eps:
-            lb,ub = create_input_bounds_tf(image, ep)
+            lb,ub = create_input_bounds_tf(image, ep, mean=mean, std=std, dataset=dataset)
             spec_path = f"prop_{idx}_{ep}.vnnlib"
             spec_path = os.path.join(spec_dir, spec_path)
             save_vnnlib_tf_standard(lb, ub, label, spec_path, dataset)
             counter += 1
 
     print(f"Total number of props: {counter}")
+
+
+def gen_single_props_standard_cifar10(spec_path, im, label, ep):
+    mean = np.array([0.4914,0.4822,0.4465])
+    std = np.array([0.2470,0.2435,0.2616])
+    dataset = 'CIFAR10'
+    lb,ub = create_input_bounds_tf(im, ep, mean=mean, std=std, dataset=dataset)
+    save_vnnlib_tf_standard(lb, ub, label, spec_path, dataset)
 
 def gen_single_props_standard(spec_path, im, label, ep):
     im = im.reshape(784)
@@ -48,15 +79,29 @@ def gen_single_props_oracle_guided(spec_path, im, label, ep):
     lb,ub = create_input_bounds_tf(im, ep)
     save_vnnlib_oracle_guided(lb, ub, label, spec_path)
 
-def is_classified_correctly(net_path, im_idx):
+def is_classified_correctly(net_path, im_idx, dataset='MNIST'):
+    if dataset == 'CIFAR10':
+        return is_classified_correctly_cifar10(net_path, im_idx)
+    else:
+        im, label = get_image_with_label(im_idx)
+        im = im.reshape(1,784,1).astype(np.float32)
+        session = ort.InferenceSession(net_path)
+        input_name = session.get_inputs()[0].name
+        output = session.run(None, {input_name: im})
+        predicted_class = np.argmax(output[0][0])
+
+        return predicted_class == label
+
+def is_classified_correctly_cifar10(net_path, im_idx):
     im, label = get_image_with_label(im_idx)
-    im = im.reshape(1,784,1).astype(np.float32)
+    inp = im.unsqueeze(0).numpy()
     session = ort.InferenceSession(net_path)
     input_name = session.get_inputs()[0].name
-    output = session.run(None, {input_name: im})
-    predicted_class = np.argmax(output[0][0])
-
-    return predicted_class == label
+    out = session.run(None, {input_name: inp})[0]  # shape [1,10]
+    logits = torch.from_numpy(out)
+    probs = softmax(logits)
+    _, pred = torch.max(probs, dim=1)  # Get predicted class index
+    return (pred.item() == label.item())
 
 def select_idxs_net_oracle(indexes_vs_oracles, net_path):
     selected_idxs = []
@@ -94,20 +139,23 @@ def setup_on_standard(netnames, im_idxs_file, dataset, timeout, epsilons, target
     os.makedirs(prp_path, exist_ok=True)
 
     for net in netnames:
-        orig_net_path = os.path.join(vnncomp_benchmarks_dir, 'onnx', net)
+        if dataset == 'CIFAR10':
+            orig_net_path = os.path.join(vnncomp_benchmarks_dir, 'nets', net)
+        else:
+            orig_net_path = os.path.join(vnncomp_benchmarks_dir, 'onnx', net)
         target_net_path = os.path.join(net_path, net)
         shutil.copy(orig_net_path, target_net_path)
 
     images, labels, indexes = select_images_with_labels(dataset_idxs_file=im_idxs_file, max_num_indexs=1000)
     labels = labels.reshape(-1)
     
-    gen_props_standard(prp_path, images, labels, indexes, epsilons)
+    gen_props_standard(prp_path, images, labels, indexes, epsilons, dataset=dataset)
 
     instance_lines = []
     for net in netnames:
         net_path1 = os.path.join(net_path, net)
         for idx in indexes:
-            if is_classified_correctly(net_path1, idx):
+            if is_classified_correctly(net_path1, idx, dataset=dataset):
                 for ep in epsilons:
                     prp_name = f"prop_{idx}_{ep}.vnnlib"
                     prp_path1 = os.path.join(prp_path, prp_name)
@@ -218,7 +266,6 @@ if __name__ == '__main__':
     set_images_labels(dataset=dataset, is_test_data=is_test_data)
 
     if property_type == 'fp':
-
         setup_on_oracle_guided_prop(netnames, oracle_labels_file, dataset, timeout, epsilons, target_benchmarks_dir, vnncomp_benchmarks_dir)
     else:
         setup_on_standard(netnames, dataset_idxs_file, dataset, timeout, epsilons, target_benchmarks_dir, vnncomp_benchmarks_dir)
